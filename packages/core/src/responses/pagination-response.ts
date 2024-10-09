b7846d5c7c3b077af95b07a02a040c82e5efed54
kaufon <kauanfvale@gmail.com>
2024-10-08 22:07:49 -0300

    💡feat: add pagination response

---
+++ packages/core/src/responses/pagination-response.ts
@@ -0,0 +1,15 @@
+type PaginationResponseProps<Item> = {
+  items: Item[];
+  itemCount: number;
+};
+
+export class PaginationResponse<Item> {
+  readonly items: Item[];  
+  readonly itemCount: number;
+
+  constructor({ items, itemCount }: PaginationResponseProps<Item>) {
+    this.items = items; 
+    this.itemCount = itemCount;
+  }
+}
+
