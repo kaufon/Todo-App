629598aa392ad29ddd951efe5846a93a2a72f72d
kaufon <kauanfvale@gmail.com>
2024-10-09 14:10:09 -0300

    ♻️refactor: task table

--- apps/web/src/ui/components/pages/dashboard/index.tsx
+++ apps/web/src/ui/components/pages/dashboard/index.tsx
@@ -1,12 +1,14 @@
-"use client"
-import { Table, TableCell, TableHeader } from "@nextui-org/table"
+"use client";
+import { Table, TableCell, TableHeader } from "@nextui-org/table";
 import { useDashBoardPage } from "./use-dashboard";
 import { TaskTable } from "./task-table";
 export const DashboardPage = () => {
-  const {tasks,isFetching} = useDashBoardPage()
+  const {deleteTasks, tasks, isFetching } = useDashBoardPage();
   return (
     <>
-      <TaskTable tasks={tasks} isLoading={isFetching}/>
+      <div className="flex items-center justify-center">
+        <TaskTable onDeleteTask={deleteTasks} tasks={tasks} isLoading={isFetching} />
+      </div>
     </>
   );
 };
--- apps/web/src/ui/components/pages/dashboard/task-table/index.tsx
+++ apps/web/src/ui/components/pages/dashboard/task-table/index.tsx
@@ -1,21 +1,23 @@
 import { Task } from "@core/domain";
 import {
-  Spinner,
-  Table,
-  TableBody,
-  TableCell,
-  TableColumn,
-  TableHeader,
-  TableRow,
+    Spinner,
+    Table,
+    TableBody,
+    TableCell,
+    TableColumn,
+    TableHeader,
+    TableRow,
 } from "@nextui-org/react";
+import { Trash2 } from "lucide-react";
 import { Tag } from "../../../commons/tag";
 
 type TaskTableProps = {
   isLoading: boolean;
   tasks: Task[];
+  onDeleteTask: (taskID:string) => Promise<void>
 };
 
-export const TaskTable = ({ isLoading, tasks }: TaskTableProps) => {
+export const TaskTable = ({ isLoading, tasks,onDeleteTask }: TaskTableProps) => {
   return (
     <>
       <Table
@@ -25,9 +27,9 @@ export const TaskTable = ({ isLoading, tasks }: TaskTableProps) => {
         selectionMode="single"
       >
         <TableHeader>
-          <TableColumn>Nome</TableColumn>
-          <TableColumn>Status</TableColumn>
-          <TableColumn>Ações</TableColumn>
+          <TableColumn className="text-zinc-400 text-xl">Nome</TableColumn>
+          <TableColumn className="text-zinc-400 text-xl">Status</TableColumn>
+          <TableColumn>{null}</TableColumn>
         </TableHeader>
         <TableBody
           items={tasks}
@@ -37,7 +39,7 @@ export const TaskTable = ({ isLoading, tasks }: TaskTableProps) => {
         >
           {(task) => (
             <TableRow key={task.getID}>
-              <TableCell>{task.name}</TableCell>
+              <TableCell className="text-zinc-600 text-lg">{task.name}</TableCell>
               <TableCell>
                 {task.status ? (
                   <Tag type="sucess">Ativado</Tag>
@@ -45,7 +47,9 @@ export const TaskTable = ({ isLoading, tasks }: TaskTableProps) => {
                   <Tag type="danger">Desativado</Tag>
                 )}
               </TableCell>
-              <TableCell>1</TableCell>
+              <TableCell>
+                <Trash2 className="text-zinc-400" onClick={() => onDeleteTask(task.getID) } />
+              </TableCell>
             </TableRow>
           )}
         </TableBody>
