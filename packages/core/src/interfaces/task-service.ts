646ed464468673b0c3da2ac49abb49c3f5761555
kaufon <kauanfvale@gmail.com>
2024-10-09 16:34:06 -0300

    ♻️ refactor: add register to service interface

--- packages/core/src/interfaces/task-service.ts
+++ packages/core/src/interfaces/task-service.ts
@@ -1,6 +1,8 @@
+import { Task } from "../domain/entities/Task";
 import { TaskDto } from "../dto/task-dto";
 import { ApiResponse, PaginationResponse } from "../responses";
 export interface ITaskService {
   listTask(): Promise<ApiResponse<PaginationResponse<TaskDto>>>;
   deleteTask(taskID:string): Promise<ApiResponse<void>>;
+  registerTask(task:Task): Promise<ApiResponse<void>>;
 }
