40ea93a806832bca0476457fbd0a085bf76720dd
kaufon <kauanfvale@gmail.com>
2024-10-08 22:06:12 -0300

    🚧in progress: task service

---
+++ apps/web/src/api/services/task-service.ts
@@ -0,0 +1,11 @@
+import { TaskDto } from "@core/dto/task-dto";
+import { IapiClient, ITaskService } from "@core/interfaces";
+import { PaginationResponse } from "@core/responses";
+
+export const TaskService = (apiClient: IapiClient): ITaskService => {
+  return {
+    async listTask() {
+      return await apiClient.get<PaginationResponse<TaskDto>>("/tarefas")
+    },
+  };
+};
