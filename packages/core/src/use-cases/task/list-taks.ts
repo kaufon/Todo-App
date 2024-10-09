39d93c0b2dd7af634acebef01c32a88155fbd36b
kaufon <kauanfvale@gmail.com>
2024-10-09 12:41:33 -0300

    🐛fix: list task response

--- packages/core/src/use-cases/task/list-taks.ts
+++ packages/core/src/use-cases/task/list-taks.ts
@@ -1,13 +1,19 @@
 import { List } from "../../domain/abstracts/List";
 import { Task } from "../../domain/entities";
+import { PaginationResponse } from "../../responses/pagination-response";
 
-export class listTasksUseCase extends List{
-  private _tasks: Task[]
-  constructor(tasks: Task[]){
-    super()
-    this._tasks = tasks
+export class listTasksUseCase {
+  private _tasks: Task[];
+  constructor(tasks: Task[]) {
+    this._tasks = tasks;
   }
-  public list(): Task[] {
-     return this._tasks 
+  public list() {
+    const tasks = this._tasks;
+    const TaskCount = this._tasks.length;
+
+    return new PaginationResponse({
+      items: tasks.map((task) => task.dto),
+      itemCount: TaskCount,
+    });
   }
 }
