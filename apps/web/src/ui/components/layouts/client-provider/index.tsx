ccab0cbb393ca89734e30c499cfeafc97bbd1ddc
kaufon <kauanfvale@gmail.com>
2024-10-09 14:09:38 -0300

    💡feat: add react hot toast

--- apps/web/src/ui/components/layouts/client-provider/index.tsx
+++ apps/web/src/ui/components/layouts/client-provider/index.tsx
@@ -1,12 +1,18 @@
-'use client'
+"use client";
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { ReactNode } from "react";
+import { Toaster } from "react-hot-toast";
 
 type ClientProviderProps = {
   children: ReactNode;
 };
 export function ClientProvider({ children }: ClientProviderProps) {
   const router = useRouter();
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider navigate={router.push}>
+      <Toaster position="top-left" />
+      {children}
+    </NextUIProvider>
+  );
 }
